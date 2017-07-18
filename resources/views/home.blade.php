c25710803fcfd8b138f6e7b3f992fef7790875c5
Nick <numbnick2@gmail.com>
2017-07-17 21:25:36 -0400

    updated css for home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
             <div class="col-md-6 main-text-div">
                 <p class="text-center">Out with the new, in the with old. Dust off those games in the back of your closet and get to playing.</p>
                 @if(Auth::guest())
-                    <p class="text-center">
+                    <p class="text-center main-login">
                         <a href="/login">Login</a> or <a href="/register">Register</a> to get started.
                     </p>
                 @endif
